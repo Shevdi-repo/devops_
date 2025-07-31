7acf57cd41fd4cb74e881e4f13fec9da4be77897
Shevdi-repo <shevaatdi@students.amikom.ac.id>
2025-07-31 23:19:48 +0700

    + port

--- src/app.py
+++ src/app.py
@@ -18,5 +18,8 @@ def index():
     return render_template('index.html', items=inventory_items)
 
 # Menjalankan Aplikasi
+import os # Pastikan ini ada di atas
+
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    port = int(os.environ.get('PORT', 5001))
+    app.run(debug=True, host='0.0.0.0', port=port)
